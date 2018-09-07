6f60fa7d5f3553433012f9da4675e5f321c2602d
J-Hust <33457091+J-Hust@users.noreply.github.com>
2018-09-07 10:02:53 -0500

    update text mining script

--- futurama_words.R
+++ futurama_words.R
@@ -1,10 +1,10 @@
-library(rvest)
-library(stringr)
-library(dplyr)
-library(SnowballC)
-library(tm)
-library(DT)
-library(htmlwidgets)
+library('rvest')
+library('stringr')
+library('dplyr')
+library('SnowballC')
+library('tm')
+library('DT')
+library('htmlwidgets')
 
 
 #####################################
@@ -13,29 +13,28 @@ library(htmlwidgets)
 
 
 tnodes <- c("5", "7", "9", "11")
-main <- read_html("https://theinfosphere.org/Episode_Transcript_Listing")
+transcriptList <- read_html("https://theinfosphere.org/Episode_Transcript_Listing")
 
-epmaster <- data.frame(matrix(nrow=1, ncol=5))
-colnames(epmaster) <- c("Title", "Original.airdateFOX", "Production.code", "TV.Broadcast", "X.")
+epMaster <- data.frame(matrix(nrow=1, ncol=5))
+colnames(epMaster) <- c("Title", "Original.airdateFOX", "Production.code", "TV.Broadcast", "X.")
 
 for (i in tnodes){
-  eplist <- main %>% html_nodes(paste0("table.overview:nth-child(", i, ")")) %>% html_table()
-  eplist <- as.data.frame(eplist)
-  epmaster <- rbind(epmaster, eplist)
+  episode <- transcriptList %>% html_nodes(paste0("table.overview:nth-child(", i, ")")) %>% html_table()%>% as.data.frame()
+  epMaster <- rbind(epMaster, episode)
 }
 
 #Constructing URLs for each transcript
-epmaster <-subset(epmaster, Title!="")
-epmaster$Title <- str_replace_all(epmaster$Title, " ", "_")
-epmaster$Title <- str_replace_all(epmaster$Title, "\\?", "%3F")
-epmaster$Title <- str_replace_all(epmaster$Title, "30%", "30%25")
+epMaster <-subset(epMaster, Title!="")
+epMaster$Title <- str_replace_all(epMaster$Title, " ", "_")
+epMaster$Title <- str_replace_all(epMaster$Title, "\\?", "%3F")
+epMaster$Title <- str_replace_all(epMaster$Title, "30%", "30%25")
 
 
 #Download the transcripts and combine into one object
-bigtranscript <- list()
-for (i in 1:nrow(epmaster)){
-  temptranscript <- read_html(paste0("https://theinfosphere.org/", epmaster[i,1]), encoding='UTF-8') %>% html_nodes("#mw-content-text") %>% html_text()
-  bigtranscript <- rbind(bigtranscript, temptranscript)
+fullTranscripts <- list()
+for (i in 1:nrow(epMaster)){
+  tempTranscript <- read_html(paste0("https://theinfosphere.org/", epMaster[i,1]), encoding='UTF-8') %>% html_nodes("#mw-content-text") %>% html_text()
+  fullTranscripts <- rbind(fullTranscripts, tempTranscript)
 }
 
 
@@ -45,22 +44,23 @@ for (i in 1:nrow(epmaster)){
 
 
 #Remove stage directions
-bigtranscript <- str_replace_all(bigtranscript, "\\[.+\\]", "")
+fullTranscripts <- str_replace_all(fullTranscripts, "\\[.+\\]", "")
+fullTranscripts <- str_replace_all(fullTranscripts, "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
 
 
 #These episodes have timestamps in each line.  Remove those and replace with newline to ensure consistency between all episodes
-bigtranscript[1] <- str_replace_all(bigtranscript[1], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
-bigtranscript[2] <- str_replace_all(bigtranscript[2], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
-bigtranscript[3] <- str_replace_all(bigtranscript[3], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
-bigtranscript[4] <- str_replace_all(bigtranscript[4], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
-bigtranscript[5] <- str_replace_all(bigtranscript[5], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
-bigtranscript[6] <- str_replace_all(bigtranscript[6], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
-bigtranscript[7] <- str_replace_all(bigtranscript[7], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
-bigtranscript[54] <- str_replace_all(bigtranscript[54], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
+fullTranscripts[1] <- str_replace_all(fullTranscripts[1], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
+fullTranscripts[2] <- str_replace_all(fullTranscripts[2], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
+fullTranscripts[3] <- str_replace_all(fullTranscripts[3], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
+fullTranscripts[4] <- str_replace_all(fullTranscripts[4], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
+fullTranscripts[5] <- str_replace_all(fullTranscripts[5], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
+fullTranscripts[6] <- str_replace_all(fullTranscripts[6], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
+fullTranscripts[7] <- str_replace_all(fullTranscripts[7], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
+fullTranscripts[54] <- str_replace_all(fullTranscripts[54], "\\([:digit:]{1,2}\\:[:digit:]{1,2}\\).{4}", "/n")
 
 
-#Characters in Universe A on Farnsworth Parabox (bigtranscript[71]) should have names standardized.
-bigtranscript[71] <- str_replace_all(bigtranscript[71], " A(?=\\:)", "")  
+#Characters in Universe A on Farnsworth Parabox (fullTranscripts[71]) should have names standardized.
+fullTranscripts[71] <- str_replace_all(fullTranscripts[71], " A(?=\\:)", "")
 
 
 #########################
@@ -75,16 +75,16 @@ mychars <- c("Fry", "Bender", "Leela", "Farnsworth", "Amy", "Hermes", "Zapp", "K
 #Create dataframe for each character in mychars
 for (i in 1:length(mychars)){
   nam <- paste(mychars[i], "_lines", sep="")
-  assign(nam, str_extract_all(bigtranscript, paste0("(?<=", mychars[i], "\\: ).*")))
+  assign(nam, str_extract_all(fullTranscripts, paste0("(?<=", mychars[i], "\\: ).*")))
 }
 
 
 #Word count for each character
 for (i in 1:length(mychars)){
   b <- paste0(mychars[i], '_done')
-  assign(b, get(paste0(mychars[i], "_lines")) 
-         %>% VectorSource() 
-         %>% Corpus() 
+  assign(b, get(paste0(mychars[i], "_lines"))
+         %>% VectorSource()
+         %>% Corpus()
          %>% tm_map(FUN=tolower)
          %>% tm_map(FUN = removeWords, stopwords('english'))
          %>% tm_map(FUN=stripWhitespace)
@@ -99,12 +99,14 @@ for (i in 1:length(mychars)){
 wordsByCharacter <- data.frame(matrix(nrow=1, ncol=4))
 colnames(wordsByCharacter) <- c('Character', 'Rank', 'Word', 'Frequency')
 
+
 for (i in 1:length(mychars)){
-  CharTop10 <- head(get(paste0(mychars[i], '_done'))[order(get(paste0(mychars[i], '_done'))$Rank, decreasing = F), ], n = 10)
+  tmp <- get(paste0(mychars[i], '_done'))
+  tmp <- tmp[tmp$Word!='character0', ]
+  CharBottom10 <- head(tmp[order(tmp$Rank, decreasing = F), ], n = 10)
+  wordsByCharacter <- rbind(wordsByCharacter, CharBottom10)
+  CharTop10 <- head(tmp[order(tmp$Rank, decreasing = T), ], n = 10)
   wordsByCharacter <- rbind(wordsByCharacter, CharTop10)
-}  
+}
 
-
-mydt <- datatable(wordsByCharacter, filter='top', options=list(pageLength=11, autoWidth=T), rownames=F)
-
-saveWidget(mydt, 'Futurama_words_by_character.html')
+write.csv(wordsByCharacter, 'words.csv')
